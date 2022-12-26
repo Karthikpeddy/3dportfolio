a85a928aaa79e16b57e33cf0d1340cb1887d8b90
Karthikpeddy <117649571+Karthikpeddy@users.noreply.github.com>
2022-12-26 18:14:53 +0530

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/ic9zzXov3Su4Ev1C/scene.splinecode');
